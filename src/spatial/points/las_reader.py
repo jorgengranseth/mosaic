d151bac1450906953810e122b03e892225336f01
Jørgen Granseth <jorgen.granseth@kartverket.no>
2025-11-19 09:58:22 +0100

    Log file opened

--- src/spatial/points/las_reader.py
+++ src/spatial/points/las_reader.py
@@ -75,6 +75,7 @@ class LASToGeometryDataSourceReader(DataSourceReader):
         # TODO: process files in a directory (for now supports only file)
         print(f"Open file with laspy, {chunk_size}")
         with laspy.open(input_path) as f:
+            print("File opened")
 
             # TODO: test performance of using scaled coords vs calculating them on the fly
 
